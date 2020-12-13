1d4a30f52cc0f1028ee3f483aa3750fb56f05e25
khang toh <khang.toh@foodpanda.com>
2020-12-14 01:22:42 +0800

    Flutter MVU with Dartea

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_mvu/main.dart';
+import 'package:dartea/dartea.dart';
 
 void main() {
+  final program = Program<CounterModel, Command, Null>(
+    init,
+    update,
+    view,
+  ).withDebugTrace();
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyApp(program));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
